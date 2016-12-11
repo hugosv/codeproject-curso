2a5f76d5da9faceafb73761486eb0ab79b900e06
Hugo Vieira <hugosv@gmail.com>
2016-12-11 16:48:16 -0200

    correcao - estruturando cruds

--- resources/assets/js/services/project.js
+++ resources/assets/js/services/project.js
@@ -1,10 +1,11 @@
 angular.module('app.services')
     .service('Project', ['$resource', '$filter', '$httpParamSerializer', 'appConfig',
         function ($resource, $filter, $httpParamSerializer, appConfig) {
+
             function transformData(data) {
                 if(angular.isObject(data) && data.hasOwnProperty('due_date')) {
                     var o = angular.copy(data);
-                    o.due_date = $filter('date')(o.due_date, 'yyyy-MM-dd');
+                    o.due_date = $filter('date')(data.due_date, 'yyyy-MM-dd');
                     return appConfig.utils.transformRequest(o);
                 }
                 return data;
